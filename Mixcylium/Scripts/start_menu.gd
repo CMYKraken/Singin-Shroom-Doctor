a8bc7077550a880aca97cf142e5f025ac094935a
Sono00 <116743231+Sono00@users.noreply.github.com>
2026-06-17 17:58:08 +0100

    Level_Select

--- Mixcylium/Scripts/start_menu.gd
+++ Mixcylium/Scripts/start_menu.gd
@@ -2,10 +2,14 @@ extends Control
 
 var Input1
 var Input2
+var Input3
+var Input4
 
 func _ready():
-	$Settings_Menu/Controls_Settings/Input_1_Container/Label.text = "a"
-	$Settings_Menu/Controls_Settings/Input_2_Container/Label.text = "d"
+	$Settings_Menu/Controls_Settings/Input_1_Container/Label.text = "D"
+	$Settings_Menu/Controls_Settings/Input_2_Container/Label.text = "F"
+	$Settings_Menu/Controls_Settings/Input_3_Container/Label.text = "J"
+	$Settings_Menu/Controls_Settings/Input_4_Container/Label.text = "K"
 	$Main_Menu.visible = true
 	$Level_Select.visible = false
 	$Settings_Menu.visible = false
@@ -13,13 +17,56 @@ func _ready():
 	$Settings_Menu/General_Settings.visible = false
 	$Settings_Menu/Volume_Settings.visible = false
 	$Settings_Menu/Controls_Settings.visible = false
+	$Level_Select/Right_Side/Level_1_Preview.visible = true
+	$Level_Select/Right_Side/Level_2_Preview.visible = false
+	$Level_Select/Right_Side/Level_3_Preview.visible = false
 
-func _process(delta):
+#region Changing Controls
+func _unhandled_key_input(event: InputEvent) -> void:
 	if Input1 == true:
+		InputMap.action_erase_events("Action_1")
+		if CheckConflict(event) == false:
+			InputMap.action_add_event("Action_1",event)
+			$Settings_Menu/Controls_Settings/Input_1_Container/Label.text = event.as_text()
 			Input1 = false
+		else:
+			$Settings_Menu/Controls_Settings/Input_1_Container/Label.text = "Key Already Bound"
 	if Input2 == true:
+		InputMap.action_erase_events("Action_2")
+		if CheckConflict(event) == false:
+			InputMap.action_add_event("Action_2",event)
+			$Settings_Menu/Controls_Settings/Input_2_Container/Label.text =  event.as_text()
 			Input2 = false
+		else:
+			$Settings_Menu/Controls_Settings/Input_2_Container/Label.text = "Key Already Bound"
+	if Input3 == true:
+		InputMap.action_erase_events("Action_3")
+		if CheckConflict(event) == false:
+			InputMap.action_add_event("Action_3",event)
+			$Settings_Menu/Controls_Settings/Input_3_Container/Label.text =  event.as_text()
+			Input3 = false
+		else:
+			$Settings_Menu/Controls_Settings/Input_3_Container/Label.text = "Key Already Bound"
+	if Input4 == true:
+		InputMap.action_erase_events("Action_4")
+		if CheckConflict(event) == false:
+			InputMap.action_add_event("Action_4",event)
+			$Settings_Menu/Controls_Settings/Input_4_Container/Label.text =  event.as_text()
+			Input4 = false
+		else:
+			$Settings_Menu/Controls_Settings/Input_4_Container/Label.text = "Key Already Bound"
 
+func CheckConflict(Action):
+	if InputMap.action_has_event("Action_1", Action):
+		return true
+	if InputMap.action_has_event("Action_2", Action):
+		return true
+	if InputMap.action_has_event("Action_3", Action):
+		return true
+	if InputMap.action_has_event("Action_4", Action):
+		return true
+	return false
+#endregion
 #region Main Menu Buttons
 func _on_start_pressed():
 	$Main_Menu.visible = false
@@ -33,7 +80,6 @@ func _on_settings_pressed():
 func _on_exit_pressed():
 	get_tree().quit()
 #endregion
-
 #region Settings Menu Buttons
 #region Settings Main
 func _on_general_pressed():
@@ -52,8 +98,15 @@ func _on_return_to_menu_pressed():
 	$Main_Menu.visible = true
 	$Settings_Menu.visible = false
 	$Settings_Menu/Settings_Main.visible = false
+	$Level_Select.visible = false
 #endregion
 #region General Settings
+func _on_window_mode_button_item_selected(index: int) -> void:
+	match index:
+		0:
+			DisplayServer.WINDOW_MODE_WINDOWED
+		1:
+			DisplayServer.WINDOW_MODE_FULLSCREEN
 
 #endregion
 #region Volume Settings
@@ -64,22 +117,35 @@ func _on_sfx_slider_value_changed(value):
 	AudioServer.set_bus_volume_db(1, linear_to_db(value))
 	
 func _on_music_slider_value_changed(value):
-	AudioServer.set_bus_volume_db(3, linear_to_db(value))
+	AudioServer.set_bus_volume_db(2, linear_to_db(value))
 	
 func _on_music_beats_slider_value_changed(value):
-	AudioServer.set_bus_volume_db(2, linear_to_db(value))
+	AudioServer.set_bus_volume_db(3, linear_to_db(value))
 #endregion
 #region Controls Settings
 func _on_input_1_pressed():
-	pass # Replace with function body.
+	Input1 = true
+	$Settings_Menu/Controls_Settings/Input_1_Container/Label.text = ""
 
 func _on_input_2_pressed():
-	pass # Replace with function body.
+	Input2 = true
+	$Settings_Menu/Controls_Settings/Input_2_Container/Label.text = ""
 
+func _on_input_3_pressed() -> void:
+	Input3 = true
+	$Settings_Menu/Controls_Settings/Input_3_Container/Label.text = ""
+
+func _on_input_4_pressed() -> void:
+	Input4 = true
+	$Settings_Menu/Controls_Settings/Input_4_Container/Label.text = ""
 #endregion
 func _on_back_to_settings_pressed():
 	$Settings_Menu/Settings_Main.visible = true
 	$Settings_Menu/General_Settings.visible = false
 	$Settings_Menu/Volume_Settings.visible = false
 	$Settings_Menu/Controls_Settings.visible = false
+#endregion
+#region Level Select
+#region Level_1
+
 #endregion
