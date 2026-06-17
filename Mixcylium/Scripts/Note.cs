037ab08b6de1ea1d54d8fd10eedc166fff4d3973
DESKTOP-4AL5OEF\Chloe <chloedawnquinton@gmail.com>
2026-06-17 22:44:58 +0100

    Added note movement (Untested)

--- Mixcylium/Scripts/Note.cs
+++ Mixcylium/Scripts/Note.cs
@@ -3,21 +3,22 @@ using System;
 
 public partial class Note : Node2D
 {
-	
+	private double _Speed;
+	public Note(){}
 	public Note(double movementSpeed){
-		
+		_Speed = movementSpeed;
 	}
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		GD.Print("Note Spawned");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		
+		Position += Vector2.Down * (float)(_Speed * delta);
 	}
 
 	public void DestroyNote(double timedelay){
