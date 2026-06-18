aa9b317e1f8ff2b7c620ff2962955587ac8b0f08
DESKTOP-4AL5OEF\Chloe <chloedawnquinton@gmail.com>
2026-06-18 23:05:18 +0100

    Created static scoring utils

---
+++ Mixcylium/Scripts/ScoringUtils.cs
@@ -0,0 +1,8 @@
+public static class ScoringUtils 
+{
+	public static double PerfectInterval = 0.35d;
+	public static double EarlyLateInterval = 1.0d;
+
+	public static int PerfectScore = 555;
+	public static int EarlyLateScore = 333; 
+}
