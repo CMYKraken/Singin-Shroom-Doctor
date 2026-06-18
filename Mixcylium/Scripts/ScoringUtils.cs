7136abeb9026b02403ba6b223f500a667c3f0ab5
DESKTOP-4AL5OEF\Chloe <chloedawnquinton@gmail.com>
2026-06-18 23:49:32 +0100

    Tweaked Scoring times

--- Mixcylium/Scripts/ScoringUtils.cs
+++ Mixcylium/Scripts/ScoringUtils.cs
@@ -1,7 +1,7 @@
 public static class ScoringUtils 
 {
-	public static double PerfectInterval = 0.35d;
-	public static double EarlyLateInterval = 1.0d;
+	public static double PerfectInterval = 0.075d;
+	public static double EarlyLateInterval = 0.150d;
 
 	public static int PerfectScore = 555;
 	public static int EarlyLateScore = 333; 
